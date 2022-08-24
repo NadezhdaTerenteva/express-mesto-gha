f7ef85a4269f2b2a9a6c4f1067613446284ae611
Nadezhda Terenteva <nadya_09@inbox.ru>
2022-08-24 23:57:48 +0300

    schemas, models

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+// Слушаем 3000 порт
+const { PORT = 3000 } = process.env;
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+    useFindAndModify: false
+});
+
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(PORT, () => {
+  // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log(`App listening on port ${PORT}`)
+})
